679e30f3fbba5638a60dd47157039c00b57fbc6e
kitoha <kitoha7@gmail.com>
2025-11-06 21:54:24 +0900

    first commit

---
+++ src/test/kotlin/com/eco/eco_msa/EcoMsaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.eco.eco_msa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EcoMsaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
